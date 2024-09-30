3bedd52bf01ecb15bdb48b7503d0da4eaabc8ba9
Ronielli Andrade <ronielli.santos@sptech.school>
2024-09-30 19:51:21 -0300

    Alterando dependencias e conexão com o banco de dados da nuvem

--- src/main/java/school/sptech/DBConnetionProvider.java
+++ src/main/java/school/sptech/DBConnetionProvider.java
@@ -10,18 +10,20 @@ public class DBConnetionProvider {
     private final DataSource dataSource;
 
     public DBConnetionProvider() {
-        // Altere com suas informações.
-        String nomeUsuario = "root";
-        String senha = "senha_usuario";
-        String bancoUsado = "dbnexus";
 
-        // Altere o endereço de conexão para se conectar com a nuvem.
-        String url = "jdbc:mysql://localhost:3306/" + bancoUsado + "/" + nomeUsuario;
+        // Credenciais do banco de dados
+        String username = "root";
+        String password = "urubu100";
+        String database = "nexus";
+        String ip = "54.165.251.183";
+
+        // URL da conexão com a instancia da nuvem
+        String url = "jdbc:mysql://" + ip + "/" + database;
 
         BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl("jdbc:mysql://localhost:3306/" + bancoUsado);
-        basicDataSource.setUsername(nomeUsuario);
-        basicDataSource.setPassword(senha);
+        basicDataSource.setUrl(url);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
 
         this.dataSource = basicDataSource;
     }
